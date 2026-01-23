cbadbbe9d60785f9627505ac80183d470b1ff32a
eliemine24 <elie.gautier24@gmail.com>
2026-01-23 16:01:04 +0100

    piooooche

--- test.js
+++ test.js
@@ -1,9 +1,9 @@
-let myCards = [0, 1, 2, 2, 3, 3, 3, 4, 4, 4, 4, "special"]
+let myCards = [0, 1, 2, 3, 4, "special"]
 
 const drawCard = function() {
   const indice = Math.floor(Math.random() * myCards.length);
   console.log(indice);
-  return myCards[indice];
+  return myCards.splice(indice, 1);
 };
 
 console.log(drawCard());
