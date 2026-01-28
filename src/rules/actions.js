ad2712e7c35b208c4fe8a21c401d38c4125d4d10
TGC <thegameclub.e123@gmail.com>
2026-01-28 10:41:11 +0100

    action WIP

--- src/rules/actions.js
+++ src/rules/actions.js
@@ -1,3 +1,47 @@
-export function applyCardEffect(player, card, gameState) {
-  // à compléter selon règles Flip7
+export function applyCardEffect(Player, card) {
+  if (card=='a') {
+    Player.score=0;
+    Player.active=false;
+  } elif (card=='b') {
+    for i in range(3){
+      carte=drawcard()
+      if (Player.active=false){
+        if (card){
+          except
+          
+        }
+      }
+    }
+
+  } else {
+     if (Player.secondchance=True) {
+      const promptSync = require("prompt-sync");
+      const prompt = promptSync();
+
+      console.log("Choisis un joueur :");
+      validTargets.forEach((p, i) => {
+      console.log(`${i + 1}. ${p.name}`);
+      });
+
+      const choice = parseInt(prompt("Numéro du joueur : "), 10) - 1;
+
+      const target = validTargets[choice];
+
+      if (!target) {
+        console.log("Choix invalide.");
+        return;
+      }
+
+      console.log(`${currentPlayer.name} désigne ${target.name}`);
+
+    }
+  }
+  return [,]
+}
+function getValidTargets(players, currentPlayer) {
+  return players.filter(p =>
+    p !== currentPlayer &&
+    p.active === true &&
+    p.secondChance === false
+  );
 }
